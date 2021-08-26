0faba998fd58b8b5cf10bd8e48e332a31e223203
Paulo Azoia <pazoia.gomes@gmail.com>
2021-08-26 16:55:16 +0100

    Added commands to Main.java file:
    deposit and withdraw from bank account

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount();
+
+        bankAccount.deposit(1000);
+        System.out.println("My balance: ￡" + bankAccount.balance);
+
+        bankAccount.deposit(2000);
+        System.out.println("My balance: ￡" + bankAccount.balance);
+
+        bankAccount.withdraw(500);
         System.out.println("My balance: ￡" + bankAccount.balance);
     }
 }
